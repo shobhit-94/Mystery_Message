89060db291e25e3b5c5cd4753f71d9386137427e
shobhit-94 <shobhitkumar94hwr@gmail.com>
2025-12-25 16:11:56 +0530

    commit

--- src/schemas/signInSchema.ts
+++ src/schemas/signInSchema.ts
@@ -1,6 +1,7 @@
-import {z} from 'zod'
+import { z } from "zod";
 
-export const signInSchema=z.object({
-    username:z.string(),//username ko identifier bhi bol sakte hai
-    password:z.string()
-})
+export const signInSchema = z.object({
+  // signin kerne ke liye hum username or password hi le re hai
+  username: z.string(), //username ko identifier bhi bol sakte hai
+  password: z.string(),
+});
